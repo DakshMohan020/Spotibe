cbf5eac8b4350448aa52815e6eeab9c8cf50001a
DakshMohan020 <dakshmohan180@gmail.com>
2026-03-18 23:46:51 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import SpotibeApp from '../Spotibe.jsx'
+import SpotibeApp from './Spotibe.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
