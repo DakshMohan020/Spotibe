8f2321dafa6a07b8877d3aa1405e459d2a66d31f
DakshMohan020 <dakshmohan180@gmail.com>
2026-03-18 23:42:22 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SpotibeApp from '../Spotibe.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SpotibeApp />
+  </React.StrictMode>
+)
